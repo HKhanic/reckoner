89eebd9b0e65f82f308152c76600887a131082df
Hakan <hakancopuroglu@gmail.com>
2025-09-18 11:31:17 +0200

    add backspace image

--- script.js
+++ script.js
@@ -3,20 +3,33 @@
 function setDivideButtonWidth() {
     const divideButton = document.querySelector("#divide");
     const multiplyButton = document.querySelector("#multiply");
+    const backspaceImg = document.querySelector("#backspace-img");
 
-    console.log(divideButton.style.width);
-    console.log(multiplyButton.style.width);
+    const computedMultiply = getComputedStyle(multiplyButton);
 
-    const computedMultiply = getComputedStyle(multiplyButton).width;
+    const loweredHeight = parseInt(computedMultiply.height, 10) * 0.7;
+    backspaceImg.style.maxHeight = loweredHeight + "px";
 
-    divideButton.style.maxWidth = computedMultiply;
+    divideButton.style.maxWidth = computedMultiply.width;
 }
 
 window.addEventListener("load", setDivideButtonWidth);
 window.addEventListener("resize", setDivideButtonWidth);
 // #endregion
 
+let firstOperand;
+let lastOperator;
+let secondOperand;
 
+const reckoner = document.querySelector("#reckoner");
+
+reckoner.addEventListener("click", (e) => input(e));
+
+function input(e) {
+    const inp = e.target.closest("button");
+    if(!inp) return;
+    console.log(inp.value)
+}
 
 function operation(operand1, operator, operand2) {
     switch(operator){
